4a2bf5cdbd1a6a24bea55cc6643f3c788c242efb
robert <nowak021@gmail.com>
2017-08-05 15:02:25 +0200

    initial commit

---
+++ src/main/java/robert/oauth2jwt/Oauth2JwtDemoApplication.java
@@ -0,0 +1,12 @@
+package robert.oauth2jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2JwtDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2JwtDemoApplication.class, args);
+	}
+}
