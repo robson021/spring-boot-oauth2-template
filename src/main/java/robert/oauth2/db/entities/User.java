ed1ac9b657aeb82405ecec4b59e5bfde55a117f6
robert <nowak021@gmail.com>
2017-08-08 22:59:36 +0200

    refactor userDetailsProvider

--- src/main/java/robert/oauth2/db/entities/User.java
+++ src/main/java/robert/oauth2/db/entities/User.java
@@ -1,73 +1,80 @@
 package robert.oauth2.db.entities;
 
-import javax.persistence.*;
-import javax.validation.constraints.Pattern;
 import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.Pattern;
 
 @Entity
 public class User extends BaseEntity {
 
-    @Column(nullable = false)
-    private String name;
+	@Column(nullable = false)
+	private String name;
 
-    @Column(nullable = false)
-    private String surname;
+	@Column(nullable = false)
+	private String surname;
 
-    @Column(name = "EMAIL", unique = true, nullable = false)
-    @Pattern(regexp = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,6}$")
-    private String email;
+	@Column(name = "EMAIL", unique = true, nullable = false)
+	@Pattern(regexp = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,6}$")
+	private String email;
 
-    @Column(nullable = false)
-    private String password;
+	@Column(nullable = false)
+	private String password;
 
-    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    @JoinTable(name = "USER_ROLE",
-            joinColumns = @JoinColumn(name = "USER_ID", referencedColumnName = "ID"),
-            inverseJoinColumns = @JoinColumn(name = "ROLE_ID", referencedColumnName = "ID"))
-    private Set<Role> roles;
+	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JoinTable(name = "USER_ROLE",
+			joinColumns = @JoinColumn(name = "USER_ID", referencedColumnName = "ID"),
+			inverseJoinColumns = @JoinColumn(name = "ROLE_ID", referencedColumnName = "ID"))
+	private Set<Role> roles;
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getSurname() {
-        return surname;
-    }
+	public String getSurname() {
+		return surname;
+	}
 
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    public Set<Role> getRoles() {
-        return roles;
-    }
+	public Set<Role> getRoles() {
+		return roles;
+	}
 
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
 
-    @Override
-    public String toString() {
-        return "User{" + "name='" + name + '\'' + ", surname='" + surname + '\'' + ", email='" + email + '\'' + ", password='" + password + '\'' + ", roles=" + roles + '}';
-    }
+	@Override
+	public String toString() {
+		return "User{" + "name='" + name + '\'' + ", surname='" + surname + '\'' + ", email='" + email + '\'' + ", password='" + password + '\'' + ", roles="+ roles + '}';
+	}
 }
--- src/main/java/robert/oauth2/web/svc/UserDetailsProviderImpl.java
+++ src/main/java/robert/oauth2/web/svc/UserDetailsProviderImpl.java
@@ -2,6 +2,7 @@ package robert.oauth2.web.svc;
 
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
 import robert.oauth2.web.security.userdetails.UserDetailsImpl;
 import robert.oauth2.web.svc.api.UserDetailsProvider;
 
@@ -10,20 +11,20 @@ public class UserDetailsProviderImpl implements UserDetailsProvider {
 
 	@Override
 	public UserDetailsImpl getUserDetails() {
-		return (UserDetailsImpl) SecurityContextHolder.getContext()
+		Object principal = SecurityContextHolder.getContext()
 				.getAuthentication()
 				.getPrincipal();
+
+		return (UserDetailsImpl) principal;
 	}
 
 	@Override
 	public long getUserId() {
-		return this.getUserDetails()
-				.getUserId();
+		return getUserDetails().getUserId();
 	}
 
 	@Override
 	public String getUserEmail() {
-		return this.getUserDetails()
-				.getUsername();
+		return getUserDetails().getUsername();
 	}
 }
